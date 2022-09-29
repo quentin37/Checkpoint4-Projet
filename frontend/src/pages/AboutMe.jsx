62062c9ac458cf13d3540f500cb07c8e795a14ba
quentin <quentin.billac@free.fr>
2022-09-29 14:00:10 +0200

    ajout react helmet

--- frontend/src/pages/AboutMe.jsx
+++ frontend/src/pages/AboutMe.jsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet-async";
 import ViewProfil from "../components/ViewProfil";
 
 function AboutMe() {
   return (
     <section>
+      <Helmet>
+        <title>Portfolio|About Me</title>
+      </Helmet>
       <ViewProfil />
     </section>
   );
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 import ContactForm from "../components/ContactForm";
 
 function Contact() {
   return (
     <section>
+      <Helmet>
+        <title>Portfolio|Contact</title>
+      </Helmet>
       <ContactForm />
     </section>
   );
